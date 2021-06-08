ea5f1032f64392fe322803f6af169d707e53ada6
Yura Kohut <yurakohut@Yuras-MBP.lan>
2021-06-08 20:27:50 +0300

    [ADD] test with checkboxes && radio buttons

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -77,7 +77,7 @@ describe("Our first suite", () => {
       .should("contain", "checked");
   });
 
-  it.only("assert property", () => {
+  it("assert property", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
@@ -91,5 +91,38 @@ describe("Our first suite", () => {
           .invoke("prop", "value")
           .should("contain", "Jun 17, 2021");
       });
+  });
+
+  it("radio button", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first() // or eq(0)
+          .check({ force: true })
+          .should("be.checked");
+
+        cy.wrap(radioButtons).eq(1).check({ force: true }).should("be.checked");
+
+        cy.wrap(radioButtons).eq(0).should("not.be.checked");
+
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it.only("check boxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    // only with input elements = and only check checkbox but not uncheck
+    cy.get('[type="checkbox"]').check({ force: true });
+
+    // for check and uncheck we use click()
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
   });
 });
